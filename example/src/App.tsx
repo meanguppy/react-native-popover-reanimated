5d6fd54e7189882dbbe9685ef8d8e704c54f6f81
Joel Nuesch <joel@meanguppy.com>
2023-09-23 00:54:33 -0700

    feat: initial popover implementation

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,69 @@
-import * as React from 'react';
-
+import React, { useState, useCallback } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-popover-reanimated';
-
-export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
-
-  return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
-  );
-}
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Button } from 'react-native';
+import {
+  ClosePopover,
+  PopoverManager,
+  usePopoverView,
+} from 'react-native-popover-reanimated';
 
 const styles = StyleSheet.create({
+  flex: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
   box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+    padding: 24,
+    borderRadius: 8,
+    backgroundColor: '#ccc',
+  },
+  btn: {
+    width: 100,
+    height: 50,
+    backgroundColor: 'green',
   },
 });
+
+function MyComponent() {
+  const [counter, setCounter] = useState(0);
+
+  const renderContent = useCallback(
+    (closePopover: ClosePopover) => {
+      return (
+        <View style={styles.box}>
+          <Button title="Increment" onPress={() => setCounter((v) => v + 1)} />
+          <Text>{counter}</Text>
+          <Button title="Close" onPress={closePopover} />
+        </View>
+      );
+    },
+    [counter]
+  );
+
+  const { viewRef, openPopover } = usePopoverView(renderContent);
+
+  return (
+    <View style={styles.container}>
+      <View ref={viewRef} collapsable={false}>
+        <Button onPress={openPopover} title="Open popover" />
+      </View>
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <GestureHandlerRootView style={styles.flex}>
+      <PopoverManager>
+        <MyComponent />
+        <MyComponent />
+        <MyComponent />
+      </PopoverManager>
+    </GestureHandlerRootView>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,175 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
+import React, {
+  useState,
+  useCallback,
+  useContext,
+  useMemo,
+  useRef,
+  useEffect,
+  type ReactNode,
+  type RefObject,
+} from 'react';
+import { LayoutChangeEvent } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Animated, {
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated';
+
+export type PopoverProps = {
+  children: ReactNode;
+};
+
+export type PopoverContextValue = null | {
+  updateIfActive(viewRef: RefObject<View>, renderContent: RenderContent): void;
+  open(viewRef: RefObject<View>, renderContent: RenderContent): void;
+  close(): void;
+};
+
+export type ClosePopover = () => void;
+
+export type RenderContent = (closePopover: ClosePopover) => ReactNode;
+
+export const PopoverContext = React.createContext<PopoverContextValue>(null);
+PopoverContext.displayName = 'PopoverContext';
+
+export const usePopoverView = (renderContent: RenderContent) => {
+  const context = useContext(PopoverContext);
+  const viewRef = useRef<View>(null);
+
+  useEffect(() => {
+    context?.updateIfActive(viewRef, renderContent);
+  }, [context, renderContent]);
+
+  const openPopover = useCallback(() => {
+    context?.open(viewRef, renderContent);
+  }, [context, renderContent]);
+
+  const closePopover = useCallback(() => {
+    context?.close();
+  }, [context]);
+
+  return { viewRef, openPopover, closePopover };
+};
+
+export const PopoverManager = ({ children }: PopoverProps) => {
+  const activeRef = useRef<null | View>(null);
+  const [activeRenderContent, setActiveRenderContent] =
+    useState<null | RenderContent>(null);
+
+  const hidden = useSharedValue(true);
+  const viewWidth = useSharedValue(Infinity);
+  const viewHeight = useSharedValue(Infinity);
+  const positionX = useSharedValue(0);
+  const positionY = useSharedValue(0);
+  const contentWidth = useSharedValue(0);
+  const contentHeight = useSharedValue(0);
+
+  const context = useMemo(
+    () => ({
+      updateIfActive(
+        viewRef: React.RefObject<View>,
+        renderContent: RenderContent
+      ) {
+        if (!viewRef.current || !activeRef.current) return;
+        if (viewRef.current !== activeRef.current) return;
+        setActiveRenderContent(() => renderContent);
+      },
+      open(viewRef: React.RefObject<View>, renderContent: RenderContent) {
+        viewRef.current?.measure((_x, _y, _w, _h, px, py) => {
+          activeRef.current = viewRef.current;
+          hidden.value = true;
+          positionX.value = px;
+          positionY.value = py;
+          setActiveRenderContent(() => renderContent);
+        });
+      },
+      close() {
+        activeRef.current = null;
+        hidden.value = true;
+        setActiveRenderContent(() => null);
+      },
+    }),
+    [hidden, positionX, positionY]
+  );
+
+  const closePopover = useCallback(() => {
+    context.close();
+  }, [context]);
+
+  const spy = Gesture.Manual().onTouchesDown((_, manager) => {
+    'worklet';
+    manager.fail();
+    runOnJS(closePopover)();
+  });
+
+  const onLayoutView = useCallback(
+    (evt: LayoutChangeEvent) => {
+      const { layout } = evt.nativeEvent;
+      viewWidth.value = layout.width;
+      viewHeight.value = layout.height;
+    },
+    [viewHeight, viewWidth]
+  );
+
+  const onLayoutContent = useCallback(
+    (evt: LayoutChangeEvent) => {
+      const { layout } = evt.nativeEvent;
+      contentWidth.value = layout.width;
+      contentHeight.value = layout.height;
+      hidden.value = false;
+    },
+    [hidden, contentHeight, contentWidth]
+  );
+
+  const transform = useAnimatedStyle(() => {
+    const xMax = viewWidth.value - contentWidth.value - 16;
+    const yMax = viewHeight.value - contentHeight.value - 16;
+    return {
+      opacity: hidden.value ? 0 : 1,
+      transform: [
+        { translateX: clamp(positionX.value, 16, xMax) },
+        { translateY: clamp(positionY.value, 16, yMax) },
+      ],
+    };
+  });
+
+  return (
+    <PopoverContext.Provider value={context}>
+      <GestureDetector gesture={spy}>
+        <View style={styles.flex} onLayout={onLayoutView}>
+          {children}
+        </View>
+      </GestureDetector>
+      {activeRenderContent && (
+        <View style={styles.overlay} pointerEvents="box-none">
+          <Animated.View
+            style={[styles.floating, transform]}
+            onLayout={onLayoutContent}
+          >
+            {activeRenderContent(closePopover)}
+          </Animated.View>
+        </View>
+      )}
+    </PopoverContext.Provider>
+  );
+};
+
+const styles = StyleSheet.create({
+  flex: {
+    flex: 1,
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    zIndex: 1500,
+  },
+  floating: {
+    position: 'absolute',
+  },
+});
+
+function clamp(value: number, min: number, max: number) {
+  'worklet';
+  return Math.max(min, Math.min(value, max));
 }
